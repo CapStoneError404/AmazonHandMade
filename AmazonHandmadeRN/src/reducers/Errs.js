30f923b30c98668489dd6e068c441964c653af13
Ian Battin <ianbattin0@gmail.com>
2018-11-29 13:21:37 -0800

    Fixed error message not refreshing

--- AmazonHandmadeRN/src/reducers/Errs.js
+++ AmazonHandmadeRN/src/reducers/Errs.js
@@ -2,7 +2,7 @@ export default function Errs(state = [], action) {
   console.log("Errs reducing action " + action.type);
   switch (action.type) {
     case 'ERROR':
-      return state.concat(action.error.message);
+      return [action.error.message]
     case 'CLEAR_ERRS':
       return [];
     default:
