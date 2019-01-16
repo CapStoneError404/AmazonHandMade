0bc36d379348336b233ed5adfc841c62751047b0
Ian Battin <ianbattin0@gmail.com>
2019-01-16 15:35:30 -0800

    Added login with amazon button

---
+++ src/components/AmazonSignInButton/index.js
@@ -0,0 +1,57 @@
+import PropTypes from 'prop-types';
+import React, { Component } from 'react';
+import LinearGradient from 'react-native-linear-gradient'
+import { StyleSheet, Text, TouchableOpacity } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
+
+export default class AmazonSignInButton extends Component {
+  render() {
+    return (
+      <TouchableOpacity
+        style={styles.touchable}
+        onPress={this.props.onPress}>
+        <LinearGradient 
+          colors={['#FEE9AC', '#F5C543']}
+          style={styles.buttonWrapper}>
+          <Icon name='amazon' style={styles.icon} />
+          <Text style={styles.text}>
+            Login with Amazon
+              </Text>
+        </LinearGradient>
+      </TouchableOpacity>
+    );
+  }
+}
+
+AmazonSignInButton.propTypes = {
+  onPress: PropTypes.func.isRequired
+};
+
+const styles = StyleSheet.create({
+  touchable: {
+    flex: 1,
+    marginTop: 5,
+    marginBottom: 5
+  },
+  buttonWrapper: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 30,
+    borderWidth: 1,
+    borderColor: '#A9831F'
+  },
+  icon: {
+    fontSize: 30,
+    fontFamily: 'System',
+    color: 'black',
+    marginRight: 5,
+    left: -5
+  },
+  text: {
+    fontSize: 20,
+    fontWeight: '500',
+    color: 'black'
+  }
+});
--- src/components/index.js
+++ src/components/index.js
@@ -4,6 +4,7 @@ export { default as Button } from './Button';
 export { default as Divider } from './Divider';
 export { default as FacebookSignInButton } from './FacebookSignInButton';
 export { default as GoogleSignInButton } from './GoogleSignInButton';
+export { default as AmazonSignInButton } from './AmazonSignInButton';
 export { default as Logo } from './Logo';
 export { default as ProfilePicture } from './ProfilePicture';
 export { default as UserInput } from './UserInput';
