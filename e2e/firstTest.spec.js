1aafdf7ba7ab2c11b42baa256fd71f260a9b1710
Scott Rizzo <scottrizzo@Scotts-MacBook-Pro-2.local>
2019-01-24 14:38:19 -0800

    further indenting fixing

--- e2e/firstTest.spec.js
+++ e2e/firstTest.spec.js
@@ -5,43 +5,41 @@ describe('Login Screen', () => {
   });
 
   it('HandMade logo is visible', async () => {
-   //await expect(element(by.id('create_account'))).toBeVisible();
-   await expect(element(by.id('handmade_logo'))).toBeVisible();
+    await expect(element(by.id('handmade_logo'))).toBeVisible();
   })
 
   it('create account is visible', async () => {
-   //await expect(element(by.id('create_account'))).toBeVisible();
-   await expect(element(by.id('create_account'))).toBeVisible();
+    await expect(element(by.id('create_account'))).toBeVisible();
   })
 
   it('forgot password button is visible', async () => {
-   await expect(element(by.id('forgot_password_button'))).toBeVisible();
+    await expect(element(by.id('forgot_password_button'))).toBeVisible();
   })
 
   it('email text input is visible', async () => {
-   await expect(element(by.id('email_login'))).toBeVisible();
+    await expect(element(by.id('email_login'))).toBeVisible();
   })
 
   it('password text input is visible', async () => {
-   await expect(element(by.id('password_login'))).toBeVisible();
+    await expect(element(by.id('password_login'))).toBeVisible();
   })
 
   it('submit button is visible', async () => {
-   await expect(element(by.id('submit_login'))).toBeVisible();
+    await expect(element(by.id('submit_login'))).toBeVisible();
   })
 
   it('enter login information and submit', async () => {
-   await element(by.id('email_login')).replaceText('user@user.com');
-   await element(by.id('password_login')).replaceText('password');
-   await element(by.id('submit_login')).tap();
-   await expect(element(by.id('artisan_list'))).toBeVisible();
+    await element(by.id('email_login')).replaceText('user@user.com');
+    await element(by.id('password_login')).replaceText('password');
+    await element(by.id('submit_login')).tap();
+    await expect(element(by.id('artisan_list'))).toBeVisible();
   })
 
   it('logout successfully', async () => {
-   await expect(element(by.text('Settings'))).toBeVisible();
-   await element(by.text('Settings')).tap();
-   await element(by.id('logout_button')).tap();
-   await expect(element(by.id('handmade_logo'))).toBeVisible();
+    await expect(element(by.text('Settings'))).toBeVisible();
+    await element(by.text('Settings')).tap();
+    await element(by.id('logout_button')).tap();
+    await expect(element(by.id('handmade_logo'))).toBeVisible();
 
    })
 });
