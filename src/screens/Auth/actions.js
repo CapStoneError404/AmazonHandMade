c82c98093fd49cfafa3b30d69a2f9a12bc9a8ac8
Ian Battin <ianbattin0@gmail.com>
2019-01-25 15:36:09 -0800

    Removed unnecessary console.logs

--- src/screens/Auth/actions.js
+++ src/screens/Auth/actions.js
@@ -6,12 +6,9 @@ export function amazonLogin(email, password) {
     return new Promise(async (resolve, reject) => {
       LoginWithAmazon.login((error, accessToken, profileData) => {
         if(error) {
-          console.log(error)
           resolve()
           dispatch({ type: 'ERROR', error: error })
         } else {
-          console.log("Login with Amazon successful!")
-
           fetch('https://us-central1-handmade-error-404.cloudfunctions.net/loginWithAmazon', {
             method: 'POST',
             headers: {
@@ -31,7 +28,6 @@ export function amazonLogin(email, password) {
           }).then(body => {
             console.log("Received response with token: " + body.token)
             firebase.auth().signInWithCustomToken(body.token).then(currentUser => {
-              console.log("Signed in with firebase custom auth and amazon")
               user = {
                 email: currentUser.user.email,
                 displayName: currentUser.user.displayName,
