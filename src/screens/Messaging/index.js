0280e42023a2165e9e5d4f781b62b2b55f842b99
Scott Rizzo <scottrizzo@Scotts-MacBook-Pro-2.local>
2019-01-31 16:05:43 -0800

    added new screen files

---
+++ src/screens/Messaging/index.js
@@ -0,0 +1,29 @@
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import * as actions from './actions'
+import * as globalActions from '@actions'
+
+function mapStateToProps(state) {
+  return {
+    User: state.User,
+    Artisans: state.Artisans,
+    Errors: state.Errors
+  }
+}
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({ ...actions, ...globalActions }, dispatch);
+}
+
+import { default as UnconnectedMessageList } from './MessageList';
+import { default as UnconnectedMessage } from './Message';
+
+
+const ArtisanList = connect(mapStateToProps, mapDispatchToProps)(UnconnectedMessageList)
+const AddArtisan = connect(mapStateToProps, mapDispatchToProps)(UnconnectedMessage)
+
+
+export {
+  MessageList,
+  Message
+}
