29a861890564e25f8f19a2bfeb24069378295211
Patrick Beninga <patrickbeninga@gmail.com>
2019-02-06 18:59:15 -0800

    inital function handler commit

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,13 @@
 const functions = require('firebase-functions');
-const admin = require('firebase-admin')
+const admin = require('firebase-admin');
+const twilio = require("twilio");
+var fs = require("fs");
 
+// the file for twlio account info.
+// ask patrick for it if you need it.
+// it should NOT be uploaded to github!!!!!!!!!!!!
+const twilioInfo = JSON.parse(fs.readFileSync("twilio.json"));
+const client = twilio(twilioInfo.accountSid, twilioInfo.authToken);
 // This file needs to be retrieved from
 // https://console.firebase.google.com/u/0/project/handmade-error-404/settings/serviceaccounts/adminsdk
 // it should NOT be uploaded to github!!!!!!!!!!!!
@@ -61,3 +68,67 @@ exports.loginWithAmazon = functions.https.onRequest((req, res) => {
     })
   })
 })
+// exports.sendMessage = functions.https.onCall((data, context) => {
+//   phone_numbers = data.phone_numbers
+//   let sns_sevrvice = new AWS.SNS({apiVersion: '2010-03-31'});
+//   if (!context.auth) {
+//     // Throwing an HttpsError so that the client gets the error details.
+//     throw new functions.https.HttpsError('failed-precondition', 'The function must be called ' +
+//         'while authenticated.');
+//   }
+//   for(var i = 0; i < phone_numbers.length; i++){
+//     var message = {Message:data.text, PhoneNumber:phone_numbers[i]}
+//     var message_promise = sns_sevrvice.publish(params).promise();
+//     message_promise.then(
+//       function(data) {
+//         console.log("MessageID is " + data.MessageId);
+//       }).catch(
+//         function(err) {
+//         console.error(err, err.stack);
+//       });
+//   }
+
+// });
+//https://docs.aws.amazon.com/sdk-for-javascript/v2/developer-guide/sns-examples-sending-sms.html
+exports.sendMessage = functions.https.onRequest((req, res) => {
+  var db = admin.database()
+  var phone_numbers = []
+  req.body.recipients.sort();
+  var cnvId = req.body.recipients.join("")
+  var cnvRef = db.ref("conversations").child(cnvId)
+  var msg = {author: req.body.sender, content:req.body.message}
+  if(cnvRef.key !== null){
+    cnvRef.child("messages").child(req.body.timeCreated).push(msg);
+  }else{
+    cnvRef.push({
+                  messages:{
+                    [req.body.timeCreated]:msg
+                  },
+                  participants: req.body.recipients
+                });
+    db.ref("amazonUsers").child(req.body.sender).child("conversations").child(cnvId).push()
+  }
+  for(var i = 0; i < req.body.recipients.length; i++){
+      phone_numbers.push(db.ref("artisans").child(req.body.recipients[i]).child(phoneNumber).replace(/\D/g,''));
+  }
+  
+  var x = 0
+  for(var i = 0; i < phone_numbers.length; i++){
+    sendText(phone_numbers[i], req.body.message).then(function(){
+      x++
+      if(x === phone_numbers.length){
+          res.status(200).send({cnv:cnvId, msg:{msg}});
+        }
+    });
+  }
+});
+
+sendText = function(number, msg){
+  return client.messages
+  .create({
+     body: msg,
+     from: '+15304884220',
+     to: "+"+number
+   })
+  .then(message => console.log(message.sid))
+}
