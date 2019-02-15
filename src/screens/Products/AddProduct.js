2d4160abe3be85d06dc0ca600ff2af57905a2910
Bryce <ki6bpd@gmail.com>
2019-02-14 19:49:49 -0800

    Moved lengthy categories object to another file

--- src/screens/Products/AddProduct.js
+++ src/screens/Products/AddProduct.js
@@ -2,6 +2,7 @@ import { AddImage, AsyncButton, UserInput, Wallpaper } from '@components';
 import React, { Component } from 'react';
 import { StyleSheet, ScrollView, View, Text, TextInput, Picker, Platform } from 'react-native';
 import ImagePicker from 'react-native-image-crop-picker';
+import { ProductCategories } from './ProductCategories';
 
 export default class AddProduct extends Component {
   static navigationOptions = {
@@ -96,24 +97,6 @@ export default class AddProduct extends Component {
   }
 
   render() {
-    let categories = {'Handmade Apparel': ['Activewear', 'Babysuits', 'Costumes', 'Handmade Clothing Sets', 'Handmade Hoodies & Sweatshirts', 'Handmade Lingerie & Underwear', 'Handmade Maternity', 'Handmade Outerwear', 'Handmade Sleepwear & Robes', 'Handmade Socks, Tights & Leg Warmers', 'Handmade Suits & Blazers', 'Handmade Sweaters', 'Handmade Swimwear', 'Handmade Tops & Tees', 'Jeans', 'Leggings', 'Overalls', 'Pants', 'Rompers', 'Shorts', 'Skirts', 'Wedding Dresses'], 
-    'Handmade Baby Products': ['Baby & Toddler Toys', 'Baby Care Products', 'Baby Gift Sets', 'Baby Keepsake Products', 'Child Carrier Products', 'Nursery Bedding', 'Nursery Decor', 'Nursery Furniture'], 
-    'Handmade Beauty & Personal Care': ['Baby & Child Care', 'Fragrance', 'Hair Care', 'Health Care', 'Makeup', 'Oral Care', 'Personal Care', 'Shaving & Hair Removal', 'Skin Care', 'Tools & Accessories', 'Wellness & Relaxation'], 
-    'Handmade Car Accessories': ['Car & Vehicle Accessories', 'Car & Vehicle Door Straps & Grab Handles', 'Car & Vehicle Electronic Accessories', 'Car & Vehicle Hitch Covers', 'Car & Vehicle Plates', 'Car & Vehicle Rearview Mirror Ornaments', 'Car & Vehicle Shift Knobs', 'Car & Vehicle Tire Covers', 'Golf Cart Seats & Covers', 'Vehicle Decals'], 
-    'Handmade Electronics Accessories': ['Camera Accessories', 'Cell Phone Armbands', 'Cell Phone Carrying Cases', 'Cell Phones Decals', 'Cell Phone Protecive Skins', 'Computer Cases', 'Computer Wrist Rests', 'eBook Reader Covers', 'eBook Reader Protective Skins', 'eBook Reader Sleeves', 'eBook Reader Stands', 'Fitness Tracker Accessories', 'Laptop Computer Backpacks', 'Laptop Computer Briefcases', 'Laptop Computer Decals', 'Laptop Computer Hard Case Shells', 'Laptop Computer Messenger Bags', 'Laptop Computer Mounts', 'Laptop Computer Protective Skins', 'Laptop Computer Shoulder Bags', 'Laptop Computer Sleeves', 'Laptop Computer Stands', 'Mouse Pads', 'MP3 Player Cases', 'MP3 Player Decals', 'MP3 Player Protective Skins', 'Touch Screen Tablet Computer Bags', 'Touch Screen Tablet Computer Cases', 'Touch Screen Tablet Computer Decals', 'Touch Screen Tablet Computer Mounts', 'Touch Screen Tablet Computer Protective Screens', 'Touch Screen Tablet Computer Sleeves', 'Touch Screen Tablet Computer Stands', 'Vehicle Electronics Accessories'], 
-    'Handmade Hair Accessories': ['Hair Barrettes', 'Hair Bun & Crown Shapers', 'Hair Claws', 'Hair Clips', 'Hair Comb Slides', 'Hair Elastics & Ties', 'Hair Extensions', 'Hair Pins', 'Hairpieces', 'Headbands', 'Tiaras', 'Wigs'], 
-    'Handmade Handbags & Accessories': ['Apparel Accessories', 'Handbags'], 
-    'Handmade Home & Kitchen Products': ['Artwork', 'Bath', 'Bedding', 'Cleaning Supplies', 'Furniture', 'Home Decor', 'Kitchen & Dining', 'Stationery & Party Supplies', 'Storage & Organization'], 
-    'Handmade Jewelry': ['Anklets', 'Body Jewlery', 'Bracelets', 'Brooches', 'Charms', 'Cufflinks', 'Earrings', 'Hair Jewelry', 'Jewelry Coins', 'Jewelry Pins', 'Jewelry Pouches', 'Jewelry Sets', 'Necklaces', 'Pendants', 'Ring Cushions', 'Rings', 'Shirt Studs', 'Tie Clips', 'Tie Pins', 'Wedding & Engagement Rings'], 
-    'Handmade Lighting': ['Ceiling Lighting', 'Lamps', 'Lanterns', 'Lightswitch Plates', 'Outdoor Lighting', 'Wall Lighting Fixtures'],
-    'Handmade Luggage & Travel Accessories': ['Luggage', 'Travel Accessories'], 
-    'Handmade Outdoor & Patio': ['Arbors', 'Birdbath Accessories', 'Birdbaths', 'Birdfeeder Accessories', 'Birdhouses', 'Garden Stakes', 'Outdoor Chimes', 'Outdoor Clocks', 'Outdoor Curtains', 'Outdoor Decorative Fences', 'Outdoor Decorative Stones', 'Outdoor Flag Accessories', 'Outdoor Flags', 'Outdoor Flower Pots', 'Outdoor Fountain Accessories', 'Outdoor Fountains', 'Outdoor Hanging Planters', 'Outdoor Planter Boxes', 'Outdoor Statues', 'Outdoor Sun Catchers', 'Outdoor Sundials', 'Outdoor Urns', 'Outdoor Window Boxes', 'Outdoor Yard Signs', 'Weathervanes', 'Wind Sculptures', 'Wind Spinners'], 
-    'Handmade Pet Supplies': ['Pet Apparel & Accessories', 'Pet Beds & Furniture', 'Pet Cages & Accessories', 'Pet Carriers & Travel Products', 'Pet Collars, Harnesses & Leashes', 'Pet Crates & Kennels', 'Pet Doors & Gates', 'Pet Feeding & Watering Supplies', 'Pet Flea & Tick Control', 'Pet Flood', 'Pet Grooming Supplies', 'Pet Health & Wellness', 'Pet Houses & Habitats', 'Pet Litter & Housebreaking', 'Pet Memorials & Urns', 'Pet Storage & Organization', 'Pet Toys', 'Pet Training Equipment', 'Pet Treats'], 
-    'Handmade Shoe Accessories': ['Decorative Shoe Clips', 'Insoles', 'Shoe Care Tools', 'Shoe Horns', 'Shoe Trees', 'Shoelaces', 'Shoes Bags'], 
-    'Handmade Shoes': ['Baby Booties & Crib Shoes', 'Ballet Flats', 'Boots', 'Clogs & Mules', 'First Walking Shoes', 'Loafers & Slip-Ons', 'Mary Janes', 'Oxfords', 'Pumps', 'Sandals', 'Slippers', 'Sneakers & Athletic Shoes'], 
-    'Handmade Sports & Outdoors': ['Camping & Hiking Equipment', 'Cycling Equipment', 'Fishing Equipment', 'Hunting & Shooting Equipment', 'Lawn & Playground Equipment', 'Sports & Fitness Equipment'], 
-    'Handmade Toys & Games': ['Handmade Games', 'Handmade Toys']}
-
     return (
       <Wallpaper style={styles.container}>
       <ScrollView>
@@ -130,7 +113,7 @@ export default class AddProduct extends Component {
             onValueChange={(itemValue, itemIndex) =>
               this.setState({productMainCategory: itemValue})
             }>
-            {Object.keys(categories).map(m => {
+            {Object.keys(ProductCategories).map(m => {
               return <Picker.Item key={m} label={m} value={m} />
             })}
           </Picker>
@@ -148,7 +131,7 @@ export default class AddProduct extends Component {
             }>
             {
               (this.state.productMainCategory != "")? 
-              categories[this.state.productMainCategory].map(s => {
+              ProductCategories[this.state.productMainCategory].map(s => {
                  return <Picker.Item label={s} value={s} key={s}/>
               }) : null 
             }
