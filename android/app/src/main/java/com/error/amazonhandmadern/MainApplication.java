63e40195137603df3bd5d33d851991307d19b2cc
Scott Rizzo <scottrizzo@Scotts-MacBook-Pro-2.local>
2019-02-19 11:09:44 -0800

    fixed issue with android now runs good

--- android/app/src/main/java/com/error/amazonhandmadern/MainApplication.java
+++ android/app/src/main/java/com/error/amazonhandmadern/MainApplication.java
@@ -4,11 +4,11 @@ import android.annotation.SuppressLint;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import com.oblador.vectoricons.VectorIconsPackage;
-import com.reactlibrary.RNLoginWithAmazonPackage;
-import com.BV.LinearGradient.LinearGradientPackage;
-import com.reactnative.ivpusic.imagepicker.PickerPackage;
-import io.invertase.firebase.RNFirebasePackage;
+//import com.oblador.vectoricons.VectorIconsPackage;
+//import com.reactlibrary.RNLoginWithAmazonPackage;
+// import com.BV.LinearGradient.LinearGradientPackage;
+// import com.reactnative.ivpusic.imagepicker.PickerPackage;
+// import io.invertase.firebase.RNFirebasePackage;
 import com.reactlibrary.RNLoginWithAmazonPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.BV.LinearGradient.LinearGradientPackage;
@@ -53,11 +53,11 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.asList(
         new MainReactPackage(),
-            new VectorIconsPackage(),
-            new RNLoginWithAmazonPackage(),
-            new LinearGradientPackage(),
-            new PickerPackage(),
-            new RNFirebasePackage(),
+            // new VectorIconsPackage(),
+            // new RNLoginWithAmazonPackage(),
+            // new LinearGradientPackage(),
+            // new PickerPackage(),
+            // new RNFirebasePackage(),
             new RNLoginWithAmazonPackage(),
             new VectorIconsPackage(),
             new LinearGradientPackage(),
