8e9224916ae581ca50548210a263d7ffabfbffc9
Ian Battin <ianbattin0@gmail.com>
2019-02-20 15:34:12 -0800

    Added cloud functions for messaging

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,6 +3,7 @@ import * as admin from 'firebase-admin'
 
 import { loginWithAmazon } from './auth.functions'
 import { addArtisan, deleteArtisan } from './artisan.functions'
+import { sendMessage, receiveMessage } from './messaging.functions'
 
 // This file needs to be retrieved from
 // https://console.firebase.google.com/u/0/project/handmade-error-404/settings/serviceaccounts/adminsdk
@@ -18,3 +19,5 @@ admin.initializeApp({
 exports.loginWithAmazon = functions.https.onCall(loginWithAmazon)
 exports.addArtisan = functions.https.onCall(addArtisan)
 exports.deleteArtisan = functions.https.onCall(deleteArtisan)
+exports.sendMessage = functions.https.onCall(sendMessage)
+exports.receiveMessage = functions.https.onRequest(receiveMessage)
--- src/screens/Messaging/actions.js
+++ src/screens/Messaging/actions.js
@@ -1,64 +1,74 @@
 import firebase from 'react-native-firebase'
 
 export function sendMessage(sender, message, recipients) {
+  console.log(`Sending a message from ${sender} to ${recipients} with the following content:`)
+  console.log(message)
+  
   return (dispatch, prevState) => { 
     return new Promise(async (resolve, reject) => {
-      fetch('https://us-central1-handmade-error-404.cloudfunctions.net/sendMessage', {
-        method: 'POST',
-        headers: {
-          'Accept': 'application/json',
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          sender: sender,
-          message: message,
+      let sendMessage = firebase.functions().httpsCallable('sendMessage')
+      
+      let response = await sendMessage({
+        sender: sender,
+        recipients: recipients,
+        message: {
           timeCreated: (new Date()).toDateString(),
-          recipients: recipients
-        })
-      }).then(res => {
-        return res.json()
-      }).then(res => {
-        resolve()
-        dispatch({type: 'SEND_MESSAGE', conversation: res.conversation, message: res.message})
+          contents: message
+        }        
+      })
+
+      console.log("Received data from cloud function:")
+      console.log(response)
+      
+      resolve()
+      dispatch({
+        type: 'SEND_MESSAGE', 
+        conversation: response.data.conversation, 
+        message: response.data.message
       })
     })
   }
 }
 
-export function fetchConversations() {
+export function fetchConversations(cgaID) {
+  console.log(`Fetching all conversations for cga with uid: ${cgaID}`)
+
   return (dispatch, prevState) => {
     return new Promise(async (resolve, reject) => {
-      console.log("Fetching all conversations")
-      snapshot = await firebase.database().ref('conversations').once('value')
-      
-      conversations = snapshot.val()
+      let snapshot = await firebase.database().ref(`amazonUsers/${cgaID}/conversations`).once('value')
+      let conversationIds = snapshot.val() ? Object.keys(snapshot.val()) : []
+      let conversations = (await firebase.database().ref('conversations').once('value')).val()
+
+      console.log("Retrieved the following cga conversations ids:")
+      console.log(conversationIds)
+      console.log("Retrieved the following conversations:")
+      console.log(conversations)
+
+      var conversationArray = []
+
+      for(var uid in conversations) {
+        if(conversationIds.includes(uid)) {
+          conversationArray.push({
+            uid: uid,
+            ...conversations[uid]
+          })
+        }
+      }
+
       resolve()
-      dispatch({type: 'GET_CONVERSATIONS', conversations: conversations})
+      dispatch({type: 'GET_CONVERSATIONS', conversations: conversationsArray})
     })
   }
 }
 
-export function fetchMessages(conversation) {
+export function receiveMessage(message, conversationID) {
+  console.log(`Receiving a message for the conversation with uid: ${conversationID} and the following data:`)
+  console.log(message)
+
   return (dispatch, prevState) => {
     return new Promise(async (resolve, reject) => {
-      console.log("Fetching messages")
-      snapshot = await firebase.database()
-        .ref('conversations')
-        .ref(conversation)
-        .once('value')
-
-      messages = []
-      conversationObject = snapshot.val()
-      
-      for(var uid in conversationObject.messages) {
-        messages.push({
-          ...conversationObject.messages[uid],
-          uid: uid
-        })
-      }
-      
       resolve()
-      dispatch({type: 'GET_MESSAGES', conversation: conversation, messages: messages})
+      dispatch({type: 'RECEIVE_MESSAGE', message: message, conversation: conversationID})
     })
   }
 }
