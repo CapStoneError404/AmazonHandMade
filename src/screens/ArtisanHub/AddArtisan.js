0775caa970403ac4957da0a0b2fade418fbb1d54
Bryce Vonilten <ki6bpd@gmail.com>
2019-04-16 18:29:40 -0700

    Made uploading a picture mandatory to avoid empty photo problems

--- src/screens/ArtisanHub/AddArtisan.js
+++ src/screens/ArtisanHub/AddArtisan.js
@@ -47,10 +47,10 @@ export default class AddArtisan extends Component {
       this.props.displayError("Location required")
     else if(!this.state.description)
       this.props.displayError("Please provide a brief description")
-    //else if(!this.state.profilePicturePath)
-    //  this.props.displayError("Please upload a profile picture")
+    else if(!this.state.profilePicturePath)
+      this.props.displayError("Please upload a profile picture")
 
-    return this.state.name && this.state.phoneNumber && this.state.location && this.state.description// && this.state.profilePicturePath
+    return this.state.name && this.state.phoneNumber && this.state.location && this.state.description && this.state.profilePicturePath
   }
   
   createArtisan() {
