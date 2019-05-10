603c9e14cc257d7394d165736e781ffb687d8772
Bryce Vonilten <ki6bpd@gmail.com>
2019-05-09 18:21:00 -0700

    Changed Settings page to include language picker. Also slightly adjusted the page styles

--- src/screens/Settings/Settings.js
+++ src/screens/Settings/Settings.js
@@ -1,14 +1,15 @@
-import { Button } from '@components'
+import { Button, Wallpaper } from '@components'
 import React, { Component } from 'react'
-import { ScrollView, StyleSheet, Text, View } from 'react-native'
+import { ScrollView, StyleSheet, Text, View, Picker, Platform } from 'react-native'
 import LinearGradient from 'react-native-linear-gradient'
+import I18n, {setLocale} from "../../utils/i18n"
 
-
-export default class Settings extends Component {
+export default class Settings extends Component {  
   constructor(props) {
     super(props)
 
     this.logout = this.logout.bind(this)
+    this.changeLanguage = this.changeLanguage.bind(this)
   }
 
   logout() {
@@ -16,24 +17,45 @@ export default class Settings extends Component {
       this.props.navigation.navigate("Login")
     })
   }
+  
+  changeLanguage(lang) {
+    setLocale(lang)
+    this.props.changeLanguage(lang)
+  }
 
   render() {
+    const lang = this.props.Settings.language
+    
     return (
-      <LinearGradient colors={['#AAAAAA', '#AAAAAA']} style={styles.container}>
+      <Wallpaper style={styles.container}>
         <View style={styles.textContainer}>
           <ScrollView>
-            <Text style={styles.text}>{"Settings"}</Text>
+            <Text style={styles.text}>{I18n.t("Settings.settingsTitle", {locale: lang})}</Text>
           </ScrollView>
         </View>
+        
+        <Text>{I18n.t("Settings.choseLanguage", {locale: lang})}:</Text>
+        <View style={styles.categorySection}>
+          <Picker
+            itemStyle={styles.PickerItem}
+            selectedValue={this.props.Settings.language}
+            testID="languagePicker"
+            onValueChange={(itemValue) => this.changeLanguage(itemValue)}>
+            <Picker.Item label="English" value="en" />
+            <Picker.Item label="Spanish" value="es" />
+            <Picker.Item label="French" value="fr" />
+          </Picker>
+        </View>
+        
         <Button
           testID='logout_button'
           style={styles.button}
-          title="Return to Login"
+          title={I18n.t("Settings.returnToLogin", {locale: lang})}
           color="#c14700"
           textColor="white"
           onPress={this.logout}
         />
-      </LinearGradient>
+      </Wallpaper>
     )
   }
 }
@@ -50,8 +72,33 @@ const styles = StyleSheet.create({
     justifyContent: 'flex-start',
     alignItems: 'center',
   },
+  categorySection: Platform.OS === 'ios' ? {
+    alignSelf: 'center',
+    width: '90%',
+    color: '#808080',
+    backgroundColor: 'white',
+    margin: '4%',
+    borderRadius: 10,
+    height: 200
+  } : {
+    alignSelf: 'center',
+    width: '90%',
+    color: '#808080',
+    backgroundColor: 'white',
+    margin: '4%',
+    borderRadius: 10,
+    height: 60
+  },
+  PickerItem: Platform.OS === 'ios' ? {
+    height: 200,
+    color: 'black'
+  } : {
+    height: 60,
+    color: 'black'
+  },
   text: {
-    fontSize: 20
+    color: 'white',
+    fontSize: 30
   },
   button: {
     flex: 1
